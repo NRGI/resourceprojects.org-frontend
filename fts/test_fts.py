cc8bc74ef337f3f72947f4e960f23d4171adbaac
Ben Webb <ben.webb@opendataservices.coop>
2015-10-15 10:35:51 +0100

    Run tests using our new fixtures

--- fts/test_fts.py
+++ fts/test_fts.py
@@ -1,7 +1,12 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+
+server_url = os.environ.get('SERVER_URL', 'http://127.0.0.2/')
+
 
 @pytest.fixture(scope="module")
 def browser(request):
@@ -11,14 +16,6 @@ def browser(request):
     return browser
 
 
-@pytest.fixture(scope="module")
-def server_url():
-    return "http://127.0.0.2/"
-
-
-
-
-   
 @pytest.mark.parametrize(('nav_item'), [
     ('About'),
     ('Projects'),
@@ -26,24 +23,23 @@ def server_url():
     ('Companies'),
     ('Data sources')
     ])
-def test_main_nav(server_url,browser,nav_item):
+def test_main_nav(browser,nav_item):
     browser.get(server_url)
     assert nav_item in browser.find_element_by_css_selector('.nav-collapse').text
 
 
-def test_index_page(server_url,browser):
+def test_index_page(browser):
     browser.get(server_url)
     assert "Natural Resource Governance Institute" in browser.title
     assert 'ResourceProjects.org' in browser.find_element_by_tag_name('body').text
 
-#This fails because we don't have a database set up with any data in    
-'''
+
 @pytest.mark.parametrize(('heading'), [
     ('Projects'),
     ('Companies Companies active in this country')
     ])
-def test_country_page(server_url,browser,heading):
-    browser.get(server_url + 'country/dz.html')
+def test_country_page(browser,heading):
+    browser.get(server_url + 'country/AO')
     titles = []
     #assert "Natural Resource Governance Institute" in browser.title
     section_titles = browser.find_elements_by_tag_name('h2')
@@ -51,5 +47,3 @@ def test_country_page(server_url,browser,heading):
         titles.append(h2.text)
     #assert 'Projects' in titles
     assert heading in titles
-    
- '''  
