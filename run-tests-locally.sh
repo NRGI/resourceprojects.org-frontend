41a1c46be7b9b3e92b7dea7bc8507137c82c2a88
Ben Webb <ben.webb@opendataservices.coop>
2015-10-15 15:44:27 +0100

    Use opendataservices/virtuoso as our virtuoso image

--- run-tests-locally.sh
+++ run-tests-locally.sh
@@ -3,8 +3,8 @@ set -e
 docker build -t opendataservices/resourceprojects.org-frontend .
 docker rm -f virtuoso-test || true
 docker rm -f frontend-test || true
-docker run -d -p 127.0.0.1:8887:8890 --name virtuoso-test caprenter/automated-build-virtuoso
-sleep 20s
+docker run -d -p 127.0.0.1:8887:8890 --name virtuoso-test opendataservices/virtuoso:master
+sleep 30s
 for f in ./fts/fixtures/*.ttl; do
     curl -T $f "http://127.0.0.1:8887/sparql-graph-crud-auth?graph=http://test" --digest --user dba:dba
 done
